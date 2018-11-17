f5453453bee81157c74b52f2d9650a7de3098557
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-11-17 02:26:11 +0100

    Rooms on map yay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::io::{self, Write};
 use termion::cursor::Goto;
 use termion::event::Key;
@@ -5,6 +6,7 @@ use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
 use tui::layout::{Constraint, Corner, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
+use tui::widgets::canvas::{Canvas, Line, Shape};
 use tui::widgets::{Block, Borders, List, Paragraph, Text, Widget};
 use tui::Terminal;
 use unicode_width::UnicodeWidthStr;
@@ -12,6 +14,55 @@ use unicode_width::UnicodeWidthStr;
 mod event;
 
 use crate::event::{Event, Events};
+
+pub struct BoxShape {
+    pub rect: Rect,
+    pub color: Color,
+}
+
+impl<'a> Shape<'a> for BoxShape {
+    fn color(&self) -> Color {
+        self.color
+    }
+
+    fn points(&'a self) -> Box<Iterator<Item = (f64, f64)> + 'a> {
+        let left_line = Line {
+            x1: f64::from(self.rect.x),
+            y1: f64::from(self.rect.y),
+            x2: f64::from(self.rect.x),
+            y2: f64::from(self.rect.y + self.rect.height),
+            color: self.color,
+        };
+        let top_line = Line {
+            x1: f64::from(self.rect.x),
+            y1: f64::from(self.rect.y + self.rect.height),
+            x2: f64::from(self.rect.x + self.rect.width),
+            y2: f64::from(self.rect.y + self.rect.height),
+            color: self.color,
+        };
+        let right_line = Line {
+            x1: f64::from(self.rect.x + self.rect.width),
+            y1: f64::from(self.rect.y),
+            x2: f64::from(self.rect.x + self.rect.width),
+            y2: f64::from(self.rect.y + self.rect.height),
+            color: self.color,
+        };
+        let bottom_line = Line {
+            x1: f64::from(self.rect.x),
+            y1: f64::from(self.rect.y),
+            x2: f64::from(self.rect.x + self.rect.width),
+            y2: f64::from(self.rect.y),
+            color: self.color,
+        };
+        Box::new(
+            left_line.into_iter().merge(
+                top_line
+                    .into_iter()
+                    .merge(right_line.into_iter().merge(bottom_line.into_iter())),
+            ),
+        )
+    }
+}
 
 #[derive(Debug)]
 pub enum GameEventType {
@@ -80,9 +131,30 @@ fn main() -> Result<(), io::Error> {
                 .style(Style::default().fg(Color::Yellow))
                 .block(Block::default().borders(Borders::ALL).title("Events"))
                 .render(&mut f, v_chunks[0]);
-            Block::default()
-                .title("Map")
-                .borders(Borders::ALL)
+            Canvas::default()
+                .block(Block::default().borders(Borders::ALL).title("Map"))
+                .paint(|ctx| {
+                    ctx.draw(&BoxShape {
+                        rect: Rect {
+                            x: 50,
+                            y: 50,
+                            width: 20,
+                            height: 20,
+                        },
+                        color: Color::White,
+                    });
+                    ctx.draw(&BoxShape {
+                        rect: Rect {
+                            x: 20,
+                            y: 20,
+                            width: 20,
+                            height: 20,
+                        },
+                        color: Color::White,
+                    });
+                })
+                .x_bounds([0.0, 100.0])
+                .y_bounds([0.0, 100.0])
                 .render(&mut f, h_chunks[1]);
         })?;
 
@@ -98,7 +170,10 @@ fn main() -> Result<(), io::Error> {
                     break;
                 }
                 Key::Char('\n') => {
-                    app.log.push(GameEvent { content: app.input.drain(..).collect(), game_event_type: GameEventType::Normal });
+                    app.log.push(GameEvent {
+                        content: app.input.drain(..).collect(),
+                        game_event_type: GameEventType::Normal,
+                    });
                 }
                 Key::Char(c) => {
                     app.input.push(c);
@@ -107,9 +182,12 @@ fn main() -> Result<(), io::Error> {
                     app.input.pop();
                 }
                 _ => {}
-            }
+            },
             event::Event::Tick => {
-                app.log.push(GameEvent { content: "text ".to_string(), game_event_type: GameEventType::Combat });
+                app.log.push(GameEvent {
+                    content: "text ".to_string(),
+                    game_event_type: GameEventType::Combat,
+                });
             }
         }
     }
