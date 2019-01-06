6612b06db30c08be338ecf6ce263e2028220d6af
Hans Ole Hatzel <hhatzel@gmail.com>
2019-01-06 13:22:20 +0100

    Use pest for command parsing

--- src/commands.rs
+++ src/commands.rs
@@ -2,41 +2,54 @@ use crate::action::Action;
 use crate::game_event::GameEventType;
 use crate::room::{adjacent_rooms, room_type_from_name};
 use crate::state::State;
+use pest_derive::Parser;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "command.pest"]
+struct CommandParser;
 
 static HELP_TEXT: &'static str =
     "Use one of the following commands: enter, attack, dodge, pickup, use.";
 
 pub fn try_handle_command(tokens: String, state: &State) -> Vec<Action> {
-    let mut split = tokens.split(" ");
-    let command = split.next();
-    match command {
+    let mut parse = CommandParser::parse(Rule::command, &tokens).unwrap().flatten();
+    let object = parse.clone().find(|pair| pair.as_rule() == Rule::object)
+        .map(|v| v.as_str());
+    let verb = parse.find(|pair| pair.as_rule() == Rule::verb);
+
+    match verb.map(|v| v.as_str()) {
         Some("enter") => {
-            let available_rooms = adjacent_rooms(state.current_room);
-            let room_name_parts: Vec<&str> = split.collect();
-            let room_name = room_name_parts.join(" ");
-            let maybe_room_type = room_type_from_name(&room_name);
-            match maybe_room_type {
-                Some(room_type) => {
-                    if available_rooms.contains(&room_type) || cfg!(debug_assertions) {
-                        vec![Action::Leave(state.current_room), Action::Enter(room_type)]
-                    } else {
-                        vec![Action::Message(
-                            String::from("You can't go to ") + &room_name + " from here.",
-                            GameEventType::Failure,
-                        )]
+            if let Some(room_name) = object {
+                let available_rooms = adjacent_rooms(state.current_room);
+                let maybe_room_type = room_type_from_name(&room_name);
+                match maybe_room_type {
+                    Some(room_type) => {
+                        if available_rooms.contains(&room_type) || cfg!(debug_assertions) {
+                            vec![Action::Leave(state.current_room), Action::Enter(room_type)]
+                        } else {
+                            vec![Action::Message(
+                                String::from("You can't go to ") + &room_name + " from here.",
+                                GameEventType::Failure,
+                            )]
+                        }
                     }
+                    None => vec![Action::Message(
+                        String::from("There is no room named \"") + &room_name + "\".",
+                        GameEventType::Failure,
+                    )],
                 }
-                None => vec![Action::Message(
-                    String::from("There is no room named \"") + &room_name + "\".",
+            } else {
+                vec![Action::Message(
+                    String::from("Specify a room to enter!"),
                     GameEventType::Failure,
-                )],
+                )]
             }
         }
         Some("attack") => vec![Action::Attack],
         Some("dodge") => vec![Action::Dodge],
         Some("pickup") => {
-            let item = split.next();
-            match item {
+            match object {
                 Some("crowbar") => vec![Action::PickUpCrowbar],
                 Some("keycard") => vec![Action::PickUpKeycard],
                 _ => vec![Action::Message(
@@ -46,8 +59,7 @@ pub fn try_handle_command(tokens: String, state: &State) -> Vec<Action> {
             }
         }
         Some("use") => {
-            let item = split.next();
-            match item {
+            match object {
                 Some("crowbar") => vec![Action::UseCrowbar],
                 Some("terminal") => vec![Action::UseTerminal],
                 Some("keycard") => vec![Action::UseKeycard],
